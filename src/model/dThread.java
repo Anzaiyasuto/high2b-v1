8afe62c80066d454b93d6fe824a57865444fd352
toitenu <toitenu.tenuitto@gmail.com>
2020-12-11 15:18:41 +0900

    スレッドを表すクラス"dThread"を追加した。

---
+++ src/model/dThread.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class dThread {
+	private String url;
+	private String title;
+	private Date time;
+	private List<Mutter> mutter = new ArrayList<Mutter>();
+}
